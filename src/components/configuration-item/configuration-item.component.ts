4dbc9f838be0b2efc7d9bd50b11b5f8d37aecd8a
Jake Crane <jake@jakecrane.com>
2017-07-22 00:15:36 -0500

    Added configuration-item component

--- src/components/configuration-item/configuration-item.component.ts
+++ src/components/configuration-item/configuration-item.component.ts
@@ -3,13 +3,12 @@ import { ConfigurationsService } from '../../services/configuration-service';
 import { Configuration } from '../../models/Configuration';
 
 @Component({
-    selector: 'configuration-item',
+    selector: '[configuration-item]',
     templateUrl: './configuration-item.component.html',
     styleUrls: ['./configuration-item.component.css']
 })
 export class ConfigurationItemComponent {
     @Input() configuration: Configuration;
     constructor() {
-        console.log(this.configuration.key);
     }
 }
