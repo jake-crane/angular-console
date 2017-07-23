68dbe89564c0ff46c6e2e849e33346a5cea64bcc
Jake Crane <jake@jakecrane.com>
2017-07-23 14:55:30 -0500

    Added ctrf+f input focus

--- src/app/components/page-header/page-header.component.ts
+++ src/app/components/page-header/page-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  @ViewChild('search') search;
+
+  @HostListener('document:keydown', ['$event'])
+  onKeyDown(e: KeyboardEvent) {
+    if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)) {
+      e.preventDefault();
+      this.search.nativeElement.focus();
+    }
+  }
 }
