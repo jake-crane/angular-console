b984bade082c1fc1e04e61cde0eda796d0c754b1
Jake Crane <jake@jakecrane.com>
2017-09-04 13:51:24 -0500

    Added basic mail setting parsing

--- src/app/components/communications/communications.component.ts
+++ src/app/components/communications/communications.component.ts
@@ -10,16 +10,43 @@ import { Configuration } from '../../models/Configuration';
 export class CommunicationsComponent implements OnInit {
 
   private configuration: Configuration;
-  private deliveryConfig: Configuration;
+  private smptHost = '';
+  private smtpPort = '';
+  private smtpUser = '';
+  private smtpPassword = '';
 
   constructor(private configurationService: ConfigurationService) { }
+
+  parseXML(deliveryConfig: Configuration) {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(deliveryConfig.value, 'text/xml');
+
+    const hostNode = xmlDoc.querySelector('[key="mail.smtp.host"]');
+    if (hostNode)
+      this.smptHost = hostNode.textContent;
+
+    const portNode = xmlDoc.querySelector('[key="mail.smtp.port"]');
+    if (portNode)
+      this.smtpPort = portNode.textContent;
+
+    const userNode = xmlDoc.querySelector('[key="mail.smtp.user"]');
+    if (userNode)
+      this.smtpUser = userNode.textContent;
+
+    const passwordNode = xmlDoc.querySelector('[key="mail.smtp.password"]');
+    if (passwordNode)
+      this.smtpPassword = passwordNode.textContent;
+  }
 
   ngOnInit() {
     this.configurationService.getConfigurations().subscribe(
       (configs) => {
-        this.deliveryConfig = configs.find((config) => {
+        const deliveryConfig = configs.find((config) => {
           return config.key === 'CMM_mail_properties';
         });
+        if (deliveryConfig) {
+          this.parseXML(deliveryConfig);
+        }
       }
     );
   }
