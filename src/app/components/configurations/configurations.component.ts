df45d34ed58188d3d7c1c261ac891d2993510ef3
Jake Crane <jake@jakecrane.com>
2017-11-23 17:42:31 -0600

    Use async pipe

--- src/app/components/configurations/configurations.component.ts
+++ src/app/components/configurations/configurations.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ConfigurationService } from '../../services/configuration/configuration.service';
 import { Configuration } from '../../models/Configuration';
-import {ConfigurationModalComponent} from '../configuration-modal/configuration-modal.component';
+import { ConfigurationModalComponent } from '../configuration-modal/configuration-modal.component';
 import { MatDialog } from '@angular/material';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-configurations',
@@ -12,15 +13,13 @@ import { MatDialog } from '@angular/material';
 })
 export class ConfigurationsComponent implements OnInit {
 
-  private configurations: Configuration[];
+  private observable: Observable<Configuration[]>;
 
   constructor(private configurationService: ConfigurationService,
     public dialog: MatDialog) { }
 
   ngOnInit() {
-    this.configurationService.getConfigurations().subscribe((configurations) => {
-      this.configurations = configurations;
-    });
+    this.observable = this.configurationService.getConfigurations().asObservable();
   }
 
   addConfiguration() {
